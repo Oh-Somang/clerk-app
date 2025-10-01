e7aea2eda092120ba137d4ecf5a16d4bb563a226
Oh-Somang <somang1006@gmail.com>
2025-10-01 10:43:50 +0900

    2nd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,10 +30,16 @@ export default function Header() {
             </SignedOut>
             <SignedIn>
               <Link
+                href="/repos"
+                className="text-gray-300 hover:text-white mr-4"
+              >
+                Repos
+              </Link>
+              <Link
                 href="/dashboard-s"
                 className="text-gray-300 hover:text-white mr-4"
               >
-                Dashboard
+                Dashboard-s
               </Link>
               <Link
                 href="dashboard-c"
